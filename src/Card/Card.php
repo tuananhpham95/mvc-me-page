c00f4a273aceae766e8544b8d5247ded488f669c
= <tuananh@hotmail.se>
2025-08-15 16:37:45 +0200

    add base Card class (suit, value, toString)

---
+++ src/Card/Card.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Card;
+
+class Card
+{
+    protected string $suit;
+    protected string $value;
+
+    public function __construct(string $suit, string $value)
+    {
+        $this->suit = $suit;
+        $this->value = $value;
+    }
+
+    public function getSuit(): string
+    {
+        return $this->suit;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return "{$this->value} {$this->suit}";
+    }
+}
