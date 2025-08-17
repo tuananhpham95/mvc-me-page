42fbb4ee338a7a0bf8d0f8736647b71febc301e2
= <tuananh@hotmail.se>
2025-08-18 00:56:31 +0200

    Add CardController for card game web routes and session page to display session contents

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -10,26 +10,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CardController extends AbstractController
 {
-    #[Route('/card', name: 'card_index')]
-    public function index(): Response
-    {
-        $uml = <<<EOD
-UML Class Diagram:
-- Card (suit, value, getAsString())
-  ^-- CardGraphic (extends Card, overrides getAsString() with UTF-8 symbols)
-- CardHand (cards: Card[], addCard(), getCards(), getAsString())
-- DeckOfCards (cards: Card[], suits, values, shuffle(), draw(), getCards(), getCardCount(), getSortedCards())
-Relationships:
-- CardHand composes Card (1-to-many)
-- DeckOfCards composes Card (1-to-many)
-- CardGraphic inherits from Card
-EOD;
-
-        return $this->render('card/index.html.twig', [
-            'uml' => $uml,
-        ]);
-    }
-
     #[Route('/session', name: 'session_show')]
     public function session(SessionInterface $session): Response
     {
@@ -45,60 +25,5 @@ EOD;
         $this->addFlash('success', 'Session has been cleared.');
         return $this->redirectToRoute('session_show');
     }
-
-    #[Route('/card/deck', name: 'card_deck')]
-    public function deck(SessionInterface $session): Response
-    {
-        $deck = $this->getDeckFromSession($session);
-        return $this->render('card/deck.html.twig', [
-            'cards' => $deck->getSortedCards(),
-            'cardCount' => $deck->getCardCount(),
-        ]);
-    }
-
-    #[Route('/card/deck/shuffle', name: 'card_deck_shuffle')]
-    public function shuffle(SessionInterface $session): Response
-    {
-        $deck = new DeckOfCards();
-        $deck->shuffle();
-        $session->set('deck', $deck);
-        return $this->render('card/deck.html.twig', [
-            'cards' => $deck->getCards(),
-            'cardCount' => $deck->getCardCount(),
-        ]);
-    }
-
-    #[Route('/card/deck/draw', name: 'card_deck_draw')]
-    public function draw(SessionInterface $session): Response
-    {
-        $deck = $this->getDeckFromSession($session);
-        $drawnCards = $deck->draw();
-        $session->set('deck', $deck);
-        return $this->render('card/draw.html.twig', [
-            'drawnCards' => $drawnCards,
-            'cardCount' => $deck->getCardCount(),
-        ]);
-    }
-
-    #[Route('/card/deck/draw/{number<\d+>}', name: 'card_deck_draw_number')]
-    public function drawNumber(SessionInterface $session, int $number): Response
-    {
-        $deck = $this->getDeckFromSession($session);
-        $drawnCards = $deck->draw($number);
-        $session->set('deck', $deck);
-        return $this->render('card/draw.html.twig', [
-            'drawnCards' => $drawnCards,
-            'cardCount' => $deck->getCardCount(),
-        ]);
-    }
-
-    private function getDeckFromSession(SessionInterface $session): DeckOfCards
-    {
-        $deck = $session->get('deck');
-        if (!$deck instanceof DeckOfCards) {
-            $deck = new DeckOfCards();
-            $session->set('deck', $deck);
-        }
-        return $deck;
-    }
+    
 }
