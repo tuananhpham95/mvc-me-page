dbacccd13e5a7e7655c32eff74d8ad840ac91275
= <tuananh@hotmail.se>
2025-08-19 17:22:18 +0200

    Complete Game 21 landing page and intro requirements

---
+++ src/Controller/GameController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    #[Route('/game', name: 'game_index')]
+    public function index(): Response
+    {
+        return $this->render('game/index.html.twig');
+    }
+}
