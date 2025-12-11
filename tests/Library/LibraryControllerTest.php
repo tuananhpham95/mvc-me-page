631061ca6e56a3e962825e63021056ce4a399b3c
= <tuananh@hotmail.se>
2025-12-11 16:07:54 +0100

    fixing bugs

---
+++ tests/Library/LibraryControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LibraryControllerTest extends WebTestCase
+{
+    public function testIndexPageLoads(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/library/');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testShowAllBooksPageLoads(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/library/show');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testCreateFormPageLoads(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/library/create');
+        $this->assertResponseIsSuccessful();
+    }
+}
